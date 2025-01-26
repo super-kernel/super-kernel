3b807bff73c8340636f1921f829d225034abc4cd
wheakerd <wheakerd@gmail.com>
2025-01-26 21:57:02 +0800

    cache

---
+++ app/Abstract/AbstractHttpController.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Abstract;
+
+/**
+ * @AbstractHttpController
+ * @\App\Abstract\AbstractHttpController
+ */
+abstract class AbstractHttpController
+{
+}
---
+++ app/Controller/Index/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Index;
+
+use App\Abstract\AbstractHttpController;
+
+/**
+ * @LoginController
+ * @\App\Controller\Index\LoginController
+ */
+final class LoginController extends AbstractHttpController
+{
+}
